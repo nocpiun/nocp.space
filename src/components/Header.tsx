e276ae6334f1d2fe99d980b85332041692d2977a
NriotHrreion <nriot233@outlook.com>
2023-10-22 16:52:20 +0800

    feat: Add avatar to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import Link from "@/components/Link";
 
+import AvatarImg from "@/static/avatar.jpg";
+
 const Header: React.FC = () => {
     return (
         <header className="h-96 pl-10 pr-10 pt-40 pb-16 flex">
@@ -9,17 +11,17 @@ const Header: React.FC = () => {
                 <h1 className="text-8xl font-[FiraCode-SemiBold] text-right">Hello</h1>
                 <p className="text-4xl text-right">I'm NoahHrreion</p>
             </div>
-            <div className="flex-[1.5] pl-20">
+            <div className="flex-[1.5] pl-20 space-y-5">
+                <img className="mb-7 rounded-full border-[2px] border-solid border-gray-600" src={AvatarImg} alt="avatar"/>
+
                 <p className="text-lg">👋 我是一个热爱网页开发、音乐和Minecraft的高中生, 很高兴能见到你！</p>
 
-                <p className="text-lg mt-5">关注我 :)</p>
-                <p className="mt-3">
-                    <ol className=" list-disc">
-                        <li>Github: <Link to="https://github.com/NriotHrreion" newTab>NoahHrreion</Link></li>
-                        <li>BiliBili: <Link to="https://space.bilibili.com/167995410" newTab>NoahHrreion</Link></li>
-                        <li>X: <Link to="https://twitter.com/Nriot_McPack" newTab>@Nriot_McPack</Link></li>
-                    </ol>
-                </p>
+                <p className="text-lg">关注我 :)</p>
+                <ol className=" list-disc">
+                    <li>Github: <Link to="https://github.com/NriotHrreion" newTab>NoahHrreion</Link></li>
+                    <li>BiliBili: <Link to="https://space.bilibili.com/167995410" newTab>NoahHrreion</Link></li>
+                    <li>X: <Link to="https://twitter.com/Nriot_McPack" newTab>@Nriot_McPack</Link></li>
+                </ol>
             </div>
         </header>
     );

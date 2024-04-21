8c8cbac3c1649fb72b40697695d29aa16f3395d7
NriotHrreion <nriot233@outlook.com>
2024-04-21 10:01:38 +0800

    perf: Add copy button icon

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,4 +1,7 @@
 import React, { PropsWithChildren, ReactElement, useMemo } from "react";
+import { ReactSVG } from "react-svg";
+
+import CopyIcon from "@/static/copy.svg";
 
 interface CodeBlockProps extends PropsWithChildren {
     
@@ -25,8 +28,10 @@ const CodeBlock: React.FC<CodeBlockProps> = (props) => {
     return (
         <pre className="relative">
             <button
-                className="absolute top-0 right-0 pt-2 pr-3 text-sm transition-colors hover:text-[--nocp-light-green] active:text-[--nocp-green]"
-                onClick={() => handleCopy()}>复制</button>
+                className="absolute top-0 right-0 pt-2 pr-2 text-sm transition-colors hover:text-[--nocp-light-green] active:text-[--nocp-green]"
+                onClick={() => handleCopy()}>
+                <ReactSVG src={CopyIcon}/>
+            </button>
             <span className="absolute bottom-0 right-0 pb-2 pr-3 text-[--nocp-light-gray] text-sm">{lang}</span>
 
             <code className={"lang-"+ lang +" hljs"}>{content}</code>

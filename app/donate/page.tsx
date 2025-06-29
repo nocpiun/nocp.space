e795e5863f8e085122c777399af1248963ceb0aa
NriotHrreion <nriot233@outlook.com>
2025-06-29 11:32:51 +0800

    fix: Donate image not available

--- app/donate/page.tsx
+++ app/donate/page.tsx
@@ -1,15 +1,13 @@
 import Image from "next/image";
 import { Page } from "@/components/page";
 
-import DonateQRCode from "@/assets/images/donate.png";
-
 export default function Donate() {
   return (
     <Page title="打赏" className="*:text-center">
       <p>打赏能给予我创造和开发的动力，如果你对我的项目感到满意，欢迎打赏！😎</p>
       <Image
         className="mt-10 ml-auto mr-auto shadow-md"
-        src={DonateQRCode.src}
+        src="/static/donate.png"
         alt="donate"
         width={384}
         height={384}/>
